f587001159248f9a3a83d2e202052265c3f14d22
Ian Withers <mr.bowtie989@gmail.com>
2022-03-01 21:39:45 -0500

    update: seed flight data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,34 @@
 #   Character.create(name: "Luke", movie: movies.first)
 tables = [Airport, Flight]
 airport_codes = ['SFO', 'DFW', 'PHL']
+durations = {
+  'SFO' => {
+    'DFW' => 1,
+    'PHL' => 2,
+  },
+  'DFW' => {
+    'SFO' => 1,
+    'PHL' => 3,
+  },
+  'PHL' => {
+    'SFO' => 2,
+    'DFW' => 3,
+  },
+}
+
+start = Date.today
+finish = (Date.today + 31.days)
 
 # tables.each { |table| table.delete_all }
 airport_codes.each { |code| Airport.create(code: code) }
+
+Airport.all.each do |airport|
+  destinations = Airport.where('code != ?', airport.code).all
+  destinations.each do |dest|
+    (start..finish).each do |date|
+      Flight.create(flight_duration: durations[airport.code][dest.code], departure_time: date.to_s + ' 07:00:00', departing_airport_id: airport.id, arriving_airport_id: dest.id)
+      Flight.create(flight_duration: durations[airport.code][dest.code], departure_time: date.to_s + ' 12:00:00', departing_airport_id: airport.id, arriving_airport_id: dest.id)
+      Flight.create(flight_duration: durations[airport.code][dest.code], departure_time: date.to_s + ' 17:00:00', departing_airport_id: airport.id, arriving_airport_id: dest.id)
+    end
+  end
+end
