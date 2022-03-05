6c99cbde1d5c659793c443bb59959d949daa5464
Ian Withers <mr.bowtie989@gmail.com>
2022-03-05 18:38:06 -0500

    update: create associations between bookings, flights, and passengers

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,6 +1,8 @@
 class Flight < ApplicationRecord
   belongs_to :arriving_airport, class_name: 'Airport'
   belongs_to :departing_airport, class_name: 'Airport'
+  has_many :bookings
+  has_many :passengers, through: :bookings
 
   def self.search(input)
     if input.nil?
