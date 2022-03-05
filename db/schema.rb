f5bcfccc619b1b37cf2f355026950aa5f28b9a1d
Ian Withers <mr.bowtie989@gmail.com>
2022-03-05 18:36:55 -0500

    update: add foreign key flight_id to passengers table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_05_203154) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_05_222643) do
   create_table "airports", force: :cascade do |t|
     t.string "code"
     t.datetime "created_at", null: false
@@ -39,6 +39,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_05_203154) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "booking_id"
   end
 
 end
